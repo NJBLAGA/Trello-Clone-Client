a2b5dc1bdf6353647ddb737546591dba47d7d32e
NJBLAGA <nathanblaga90@gmail.com>
2021-06-24 18:39:26 +1000

    Saved new git file

---
+++ src/utlis/validators.test.js
@@ -0,0 +1,11 @@
+import { validateInput } from "./validators";
+
+describe("validateInput", () => {
+  test("It returns false if bad words are present", () => {
+    expect(validateInput("bum")).toBe(false);
+    expect(validateInput("hello you bum")).toBe(false);
+  });
+  test("It returns true if no bad words are present", () => {
+    expect(validateInput("hello world")).toBe(true);
+  });
+});
